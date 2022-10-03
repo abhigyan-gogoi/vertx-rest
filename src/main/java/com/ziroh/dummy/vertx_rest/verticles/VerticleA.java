cb5e4842e72332a564791116539fd1ade3c056b7
abhigyan-gogoi <abhigyank.gogoi@zirohcom>
2022-10-03 12:21:49 +0530

    Added child verticles

--- src/main/java/com/ziroh/dummy/vertx_rest/verticles/VerticleA.java
+++ src/main/java/com/ziroh/dummy/vertx_rest/verticles/VerticleA.java
@@ -2,18 +2,11 @@ package com.ziroh.dummy.vertx_rest.verticles;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
 
-public class MainVerticle extends AbstractVerticle {
-  public static void main(String[] args) {
-    final Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new MainVerticle());
-  }
-
+public class VerticleA extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     System.out.println("STARTED: "+getClass().getName());
-    vertx.deployVerticle(new VerticleA());
     startPromise.complete();
   }
 }
