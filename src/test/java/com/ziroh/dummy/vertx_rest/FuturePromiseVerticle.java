41701c142c5f1960180b20d2c3fa0b72b00b98cc
abhigyan-gogoi <abhigyank.gogoi@zirohcom>
2022-10-06 11:23:40 +0530

    Implemented Future map, coordination and composite tests

--- src/test/java/com/ziroh/dummy/vertx_rest/FuturePromiseVerticle.java
+++ src/test/java/com/ziroh/dummy/vertx_rest/FuturePromiseVerticle.java
@@ -1,8 +1,11 @@
 package com.ziroh.dummy.vertx_rest;
 
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.Test;
@@ -46,7 +49,6 @@ public class FuturePromiseVerticle {
     vertx.setTimer(500, id -> {
       promise.complete("SUCCESS");
       log.debug("TIMER DONE: ");
-      context.completeNow();
     });
     final Future<String> future = promise.future();
     future.onSuccess(result -> {
@@ -54,5 +56,93 @@ public class FuturePromiseVerticle {
       context.completeNow();
     })
       .onFailure(context::failNow);
+  }
+
+  @Test
+  void future_failure (Vertx vertx, VertxTestContext context) {
+    final Promise<String> promise = Promise.promise();
+    log.debug("START: ");
+    vertx.setTimer(500, id -> {
+      promise.fail(new RuntimeException("FAILED!!"));
+      log.debug("TIMER DONE: ");
+    });
+    final Future<String> future = promise.future();
+    future.onSuccess(context::failNow)
+      .onFailure(error -> {
+        log.debug("RESULT: ", error);
+        context.completeNow();
+      });
+  }
+
+  @Test
+  void future_map (Vertx vertx, VertxTestContext context) {
+    final Promise<String> promise = Promise.promise();
+    log.debug("START: ");
+    vertx.setTimer(500, id -> {
+      promise.complete("SUCCESS");
+      log.debug("TIMER DONE: ");
+      context.completeNow();
+    });
+    final Future<String> future = promise.future();
+    future
+      .map(asString -> {
+        log.debug("Map string to JSON object");
+        return new JsonObject().put("key", asString);
+      })
+      .map(jsonObject -> new JsonArray().add(jsonObject))
+      .onSuccess(result -> {
+        log.debug("RESULT: {} of type {}", result, result.getClass().getSimpleName());
+        context.completeNow();
+      })
+      .onFailure(context::failNow);
+  }
+
+  @Test
+  void future_coordination (Vertx vertx, VertxTestContext context) {
+    vertx.createHttpServer()
+      .requestHandler(request -> log.debug("{}", request))
+      .listen(10_000)
+      .compose(server -> {
+        log.debug("Another task");
+        return Future.succeededFuture(server);
+      })
+      .compose(server -> {
+        log.debug("Even another task");
+        return Future.succeededFuture(server);
+      })
+      .onFailure(context::failNow)
+      .onSuccess(server -> {
+        log.debug("Server started on port {}", server.actualPort());
+        context.completeNow();
+      });
+  }
+
+  @Test
+  void future_composition (Vertx vertx, VertxTestContext context) {
+    var one = Promise.<Void>promise();
+    var two = Promise.<Void>promise();
+    var three = Promise.<Void>promise();
+
+    var futureOne = one.future();
+    var futureTwo = two.future();
+    var futureThree = three.future();
+
+    // All - requires all futures to succeed
+    // Any - requires any one future to succeed
+    // Join - before reporting result; all futures have to be returned (like all)
+    CompositeFuture.all(futureOne, futureTwo, futureThree)
+      .onFailure(context::failNow)
+      .onSuccess(result -> {
+        log.debug("SUCCESS: ");
+        context.completeNow();
+      });
+
+    // Complete futures
+    vertx.setTimer(500, id -> {
+      one.complete();
+      two.complete();
+//      three.fail("THREE FAILED!!!");
+      three.complete();
+    });
   }
 }
